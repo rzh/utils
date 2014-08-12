5c0c028232382c2c3114bfda969f4d3d461e3cbd
Rui Zhang <rui.zhang@mongodb.com>
2014-08-12 10:20:11 -0700

    add parser for pidstat and tests

--- go/mc/parser/parser.go
+++ go/mc/parser/parser.go
@@ -47,3 +47,56 @@ func ProcessSysbenchResult(file string) (string, []string, map[string]string) {
 	}
 	return cum, trend, att
 }
+
+type DataPoint struct {
+	ts string
+	d  string
+}
+
+// parse output from pidstat
+// return value:
+//     process string  [mongod/mongos]
+//     stats   map[string][]DataPoint
+func ParsePIDStat(file string) (string, map[string][]DataPoint) {
+	var cpu []DataPoint
+	var mem []DataPoint
+	process := ""
+	stats := make(map[string][]DataPoint)
+
+	f, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal("cannot opern file " + "test.txt")
+	}
+
+	lines := strings.Split(string(f), "\n")
+	re := regexp.MustCompile("     Time      TGID")
+
+	for i := 0; i < len(lines); i++ {
+		if re.MatchString(lines[i]) {
+			i++ // next line is what we are looking, which is for process. Not thread
+
+			if i < len(lines) {
+				// take Datapoint
+				dps := strings.Fields(lines[i])
+
+				if len(dps) != 19 {
+					// the line is either wrong format of truncated
+					log.Fatalf("Error parsing pidstat, line =(%s), wrong number of data %d",
+						lines[i], len(dps))
+				}
+
+				// now take data
+				cpu = append(cpu, DataPoint{dps[0], dps[6]})
+				mem = append(mem, DataPoint{dps[0], dps[12]})
+
+				if process == "" {
+					process = dps[18]
+				}
+			}
+		}
+	}
+
+	stats["cpu"] = cpu
+	stats["mem"] = mem
+	return process, stats
+}
--- go/mc/parser/parser_test.go
+++ go/mc/parser/parser_test.go
@@ -3,7 +3,7 @@ package parser
 import "testing"
 
 func TestProcessSysbenchResult(t *testing.T) {
-	cum, trend := ProcessSysbenchResult("test.txt")
+	cum, trend, att := ProcessSysbenchResult("test.txt")
 
 	if cum != "741.86" {
 		t.Error("Cumulative TPS is %v, want 741.86", cum)
@@ -11,5 +11,25 @@ func TestProcessSysbenchResult(t *testing.T) {
 
 	if trend[0] != "772.95" {
 		t.Error("History IPS[0] is %v, want 772.95", trend[0])
+	}
+
+	if att["test-type"] != "sysbench" {
+		t.Error("Attribute[\"test-type\"] is %v, want sysbench", att["test-type"])
+	}
+}
+
+func TestParsePIDStat(t *testing.T) {
+	s, dps := ParsePIDStat("pidstat.txt")
+
+	if s != "mongod" {
+		t.Error("Pidstat process-type is " + s + " expecting mongod")
+	}
+
+	if dps["cpu"][0].d != "91.01" {
+		t.Error("Pidstat cpu[0] is " + dps["cpu"][0].d + " expecting 91.01")
+	}
+
+	if dps["mem"][1].d != "23.29" {
+		t.Error("Pidstat mem[1] is " + dps["mem"][1].d + " expecting 23.29")
 	}
 }
